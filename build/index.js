d7f619757395dcf3d527867934bb0624c630aace
Afonso-cmd <afonso.ur@gmail.com>
2022-05-26 17:58:26 -0300

    number variable type

--- build/index.js
+++ build/index.js
@@ -1,4 +1,6 @@
 "use strict";
-let tecnologia;
-tecnologia = "JavaScript";
-console.log(tecnologia);
+let valor1;
+valor1 = 20;
+let valor2 = 10;
+let valor3 = 5;
+console.log(valor1 + valor2 + valor3);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-let tecnologia: string;/*Definindo tipo de variável*/
-tecnologia = "JavaScript";
-console.log(tecnologia);
+//Type number = (int, float, hex, binary)
+let valor1: number;
+valor1 = 20;
+
+let valor2: number =10;
+
+let valor3 = 5;//no affirmed what is number
+
+console.log(valor1+valor2+valor3);
+
