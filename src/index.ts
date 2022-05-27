384148ee652762bc629353bbb7200f550f0f665f
Afonso-cmd <afonso.ur@gmail.com>
2022-05-27 11:25:22 -0300

    boolean, array, tuplas

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
-//two variables types in one variable
-let userId: number | string;
-userId=123;
-console.log(typeof userId);//type variable
-console.log(userId);
+//###boolean###
+let exemple: boolean = true;
+//console.log(typeof exemple);
+//console.log(exemple);
 
-userId="123"
+let codeStatus: number = 123;
+//conversion of number for boolean
+exemple = Boolean(codeStatus);
+//console.log(exemple);
+//if variable no empty will true
 
-console.log(typeof userId);//type variable
-console.log(userId);
+//###array###
+//array declaration
+let numbers: number[];
+//OUTHER FORM: let numbers: Array<number>;
+//TWO TYPES: let numbers: <string | number>  OR let numbers: (string |number)[];
+//input in array
+numbers = [2,4,8,16];
+//console.log(numbers);
+//add element
+numbers.push(32);
+//console.log(numbers);
+
+//###tuplas###
+let test: [string, number];
+test = ['teste', 55];
+console.log (test);
+
+test.push("teste2", 110);
+console.log(test);
+
+test.push(220, "teste3");
+console.log(test);
