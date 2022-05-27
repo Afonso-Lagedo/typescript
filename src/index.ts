1652c284ceeb52350cce6653d07daf98ea491043
Afonso-cmd <afonso.ur@gmail.com>
2022-05-27 11:53:41 -0300

    ENUM, NULL, Undefined, Unknown, Assertions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,54 @@
-//Type Object
-let user: object;
+//Type ENUM
 
-user ={
-    nome:"Afonso",
-    idade:25
+enum Colors{
+    white ="#FFF",
+    black ="#000"
 }
 
-console.log(user);
+console.log(Colors.black);
+
+//Example two
+enum Status{
+    ADMIN,
+    USER,
+    STAFF
+}
+
+console.log(Status.ADMIN);
+console.log(Status.USER);
+console.log(Status.STAFF);
+
+//Example three
+enum StatusTwo{
+    ADMIN = 2,
+    USER = 0,
+    STAFF = 1
+}
+
+console.log(StatusTwo.ADMIN);
+console.log(StatusTwo.USER);
+console.log(StatusTwo.STAFF);
+
+//###Type NULL###
+let nome: null;
+//No input value
+
+//###Undefined###
+let example;
+console.log(example);
+
+//###Unknown###
+let a: unknown;
+a=2;
+console.log(a);
+a="b";
+console.log(a);
+
+//###Assertions###
+let c: unknown = 1;
+let mudaC: number = 2;
+console.log(mudaC);
+
+//mudaC = C; IT IS ERROR
+mudaC = c as number;//IT IS OK;
+console.log(mudaC);
