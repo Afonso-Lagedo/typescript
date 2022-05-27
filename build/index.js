9dcd0332a67035454e110477f6f18b2b77a48bd9
Afonso-cmd <afonso.ur@gmail.com>
2022-05-27 11:31:06 -0300

    object

--- build/index.js
+++ build/index.js
@@ -1,14 +1,7 @@
 "use strict";
-let exemple = true;
-let codeStatus = 123;
-exemple = Boolean(codeStatus);
-let numbers;
-numbers = [2, 4, 8, 16];
-numbers.push(32);
-let test;
-test = ['teste', 55];
-console.log(test);
-test.push("teste2", 110);
-console.log(test);
-test.push(220, "teste3");
-console.log(test);
+let user;
+user = {
+    nome: "Afonso",
+    idade: 25
+};
+console.log(user);
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,9 @@
-//###boolean###
-let exemple: boolean = true;
-//console.log(typeof exemple);
-//console.log(exemple);
+//Type Object
+let user: object;
 
-let codeStatus: number = 123;
-//conversion of number for boolean
-exemple = Boolean(codeStatus);
-//console.log(exemple);
-//if variable no empty will true
+user ={
+    nome:"Afonso",
+    idade:25
+}
 
-//###array###
-//array declaration
-let numbers: number[];
-//OUTHER FORM: let numbers: Array<number>;
-//TWO TYPES: let numbers: <string | number>  OR let numbers: (string |number)[];
-//input in array
-numbers = [2,4,8,16];
-//console.log(numbers);
-//add element
-numbers.push(32);
-//console.log(numbers);
-
-//###tuplas###
-let test: [string, number];
-test = ['teste', 55];
-console.log (test);
-
-test.push("teste2", 110);
-console.log(test);
-
-test.push(220, "teste3");
-console.log(test);
+console.log(user);
