300146805890375f8faa5c90e7bf7db90cc04bd8
Afonso-cmd <afonso.ur@gmail.com>
2022-07-18 16:26:32 -0300

    POO class method

--- build/index.js
+++ build/index.js
@@ -4,7 +4,30 @@ class Store {
         this.name = name;
         this.category = category;
     }
+    newStore() {
+        console.log(`Store: ${this.name}, Category: ${this.category}`);
+    }
+    newRequeset(table) {
+        return ` Table: ${table}`;
+    }
+    products(...products) {
+        products.map((products) => {
+            console.log(`Item: ${products}`);
+        });
+    }
+    status(status) {
+        if (status === "OPEN") {
+            console.log("STORE OPEN");
+        }
+        else {
+            console.log("STORE CLOSED");
+        }
+    }
 }
 const bk = new Store("BK", "Burguer");
-console.log(bk);
-console.log(bk.name);
+bk.newStore();
+const newRequeset = bk.newRequeset(7);
+console.log(newRequeset);
+const test = bk.products("Hamburguer", "Juice", "Chocolate");
+console.log(test);
+bk.status("CLOSED");
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,54 @@
 /*
 POO
 */
+type TypeStatus = "OPEN" | "CLOSED";
 
 class Store{
+    //attributes
     name:string;
     category: string;
 
+    //constructor
     constructor(name:string, category:string){
         this.name= name;
         this.category = category;
+    }
+
+    //method without paramers
+    newStore(){
+        console.log(`Store: ${this.name}, Category: ${this.category}`);
+    }
+
+    //method with paramers
+    newRequeset(table:number): string{
+        return` Table: ${table}`;
+    }
+
+    //method unknown amount of attributes
+    products(...products: string[]){
+        products.map((products) => {
+            console.log(`Item: ${products}`)
+        })
+    }
+
+    status(status:TypeStatus):void{
+        if(status === "OPEN"){
+            console.log("STORE OPEN");
+        }
+        else{
+            console.log("STORE CLOSED");
+        }
     }
 }
 
 const bk = new Store("BK", "Burguer");
 
-console.log(bk)
-console.log(bk.name)
+bk.newStore();
+
+const newRequeset = bk.newRequeset(7);
+console.log(newRequeset);
+
+const test = bk.products("Hamburguer", "Juice", "Chocolate"); 
+console.log(test);
+
+bk.status("CLOSED");
